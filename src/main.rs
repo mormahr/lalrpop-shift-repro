3f3566242581a71c82a1bb62aac94ec804b2dbde
Moritz Mahringer <me@moritzmahringer.de>
2022-01-03 20:54:19 +0100

    Add example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(
+    #[allow(clippy::all)]
+    test,
+    "/test.rs"
+);
+
 fn main() {
-    println!("Hello, world!");
+    let parser = test::StmtsParser::new();
+    let result = parser.parse("5-5");
+    println!("{:?}", result);
 }
